c55c7618605ee396bb778e340c806e253b377351
Kevin Johnson <seriot@gmail.com>
2014-06-20 15:33:52 -0400

    Create delete.php

---
+++ project2/delete.php
@@ -0,0 +1,14 @@
+<?php
+	$con=mysqli_connect('localhost','seriot','loop900','octagon');
+	// Check connection
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	if ( isset($_POST['remove']))
+	{
+		$idd = $_POST['itemdelete'];
+		mysqli_query($con,"DELETE FROM todo_t WHERE ID='".$idd."'");
+	}
+	mysqli_close($con);
+	header("Location: http://localhost/raw4/project2_octagon.php");
+	?>
