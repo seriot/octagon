c8475e0e5c1ada728461c4618d369ad0b8214909
Kevin Johnson <seriot@gmail.com>
2014-06-20 15:34:16 -0400

    Update delete.php

--- project2/delete.php
+++ project2/delete.php
@@ -1,5 +1,5 @@
 <?php
-	$con=mysqli_connect('localhost','seriot','loop900','octagon');
+	$con=mysqli_connect('localhost','username','password','octagon');
 	// Check connection
 	if (mysqli_connect_errno()) {
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
